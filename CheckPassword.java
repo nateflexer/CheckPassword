4a9046580244f80915280abaa89bdac8b01ce448
Nate Flexer <90048404+nateflexer@users.noreply.github.com>
2023-01-31 15:40:08 -0500

    Add files via upload

---
+++ CheckPassword.java
@@ -0,0 +1,37 @@
+import java.util.*;  
+import java.lang.String;  
+ 
+
+public class CheckPassword {  
+   public static void main(String[] args) {  
+      Scanner sc = new Scanner(System.in);  
+      System.out.print("Enter your password: ");  
+      String password = sc.next();  
+      if (goodPass(password)) {  
+         System.out.println("True");  
+      } else {  
+         System.out.println("False");  
+      }  
+   }  
+
+   public static boolean goodPass(String password) {  
+      if (password.length() < 8) {   
+         return false;  
+      } else {      
+         char char1;  
+         int count = 1;   
+         for (int i = 0; i < password.length() - 1; i++) {  
+            char1 = password.charAt(i);  
+            if (!Character.isLetterOrDigit(char1)) {          
+               return false;  
+            } else if (Character.isDigit(char1)) {  
+               count++;  
+               if (count < 2)   {     
+                  return false;  
+               }     
+            }  
+         }  
+      }  
+      return true;  
+   }  
+}
